123cef24dfc1dfd995699f6efe344046be35eb3c
Yonatan <yonatan@backbox.co>
2020-01-30 22:47:54 +0200

    F5 mock configuration now exports as json to web client, using axios as ajax + promise library Added missing .babelrc file

--- web/app/app.js
+++ web/app/app.js
@@ -1,19 +1,44 @@
 import React from 'react';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.css'
-import "./app.styl"
+import axios from 'axios';
+import "./app.styl";
+
+import f5_logo from '../assets/f5_logo.png';
+import engine_logo from '../assets/engine_logo.png';
 
 class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            f5MockJson: "Loading...",
+            payloadJson: "",
+            results: []
+        };
+
+        this.fetchF5Mock();
+    }
+
+     async fetchF5Mock() {
+        let result = await axios.get(window.location.origin + '/api/f5Config');
+        this.setState({f5MockJson: JSON.stringify(result.data, null, 4)});
+    };
+
     render() {
         return (
             <div className="app">
                 <h1>Operation Engine user interface!</h1>
                 <div className="container bg-dark contents">
-                    <div className="col">
-                        <textarea>This text area should contain sample raw execution configs</textarea>
+                    <div className="card">
+                        <img className="card-img-top" src={f5_logo} alt="F5 logo" />
+                        <div className="card-body">
+                            <textarea className="form-control" value={this.state.f5MockJson} />
+                        </div>
                     </div>
-                    <div className="col">
-                        <textarea>This text area should contain operation results</textarea>
+
+                    <div className="card">
+                        <img className="card-img-top" src={engine_logo} alt="F5 logo" />
+                        <div className="card-body">
+                            <textarea  className="form-control">This text area should contain operation results</textarea>
+                        </div>
                     </div>
                 </div>
             </div>
--- web/index.js
+++ web/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from './app/app';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 import "./index.styl";
 
 let mountNode = document.getElementById("app");
