60d650cdf865b808113e109c4457ae85f06af6c4
Yonatan <yonatan@backbox.co>
2020-02-10 18:40:01 +0200

    Can now execute operations (wrapped with RawExecutionConfig) through the api Basic json validations in client side (json format and presence of the most important keys)

--- src/main/java/com/SixSense/web/OperationController.java
+++ src/main/java/com/SixSense/web/OperationController.java
@@ -13,10 +13,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.Map;
@@ -62,24 +59,18 @@ public class OperationController {
     }
 
     @PostMapping("/execute")
-    public String execute(){
-        try {
-            RawExecutionConfig rawExecutionConfig = PolymorphicJsonMapper.deserialize(TestingMocks.rawExecutionConfigJsonString(), RawExecutionConfig.class);
-            ParallelWorkflow workflow = CommandUtils.composeWorkflow(rawExecutionConfig);
+    public RawExecutionConfig execute(@RequestBody RawExecutionConfig rawExecutionConfig){
+        ParallelWorkflow workflow = CommandUtils.composeWorkflow(rawExecutionConfig);
 
-            Map<String, ExpressionResult> workflowResult = workflowManager.executeWorkflow(workflow).join();  //key: operation id, value: operation result
-            for(Operation operation : workflow.getParallelOperations()) {
-                ExpressionResult result = workflowResult.get(operation.getUUID());
-                rawExecutionConfig.addResult(operation.getDynamicFields().get("device.internal.id"), result);
-                logger.info("Operation(s) " + operation.getOperationName() + " Completed with result " + result.getOutcome());
-                logger.info("Result Message: " + result.getMessage());
-            }
-
-            return wrapForHtml(PolymorphicJsonMapper.serialize(rawExecutionConfig));
-        } catch (Exception e) {
-            logger.error("A fatal exception was encountered - failed to run operation", e);
-            return "A fatal exception was encountered, check the logs for a full stack trace";
+        Map<String, ExpressionResult> workflowResult = workflowManager.executeWorkflow(workflow).join();  //key: operation id, value: operation result
+        for(Operation operation : workflow.getParallelOperations()) {
+            ExpressionResult result = workflowResult.get(operation.getUUID());
+            rawExecutionConfig.addResult(operation.getDynamicFields().get("device.internal.id"), result);
+            logger.info("Operation(s) " + operation.getOperationName() + " Completed with result " + result.getOutcome());
+            logger.info("Result Message: " + result.getMessage());
         }
+
+        return rawExecutionConfig;
     }
 
     private static String wrapForHtml(String text){
--- web/app/app.js
+++ web/app/app.js
@@ -6,12 +6,14 @@ import f5_logo from '../assets/f5_logo.png';
 import engine_logo from '../assets/engine_logo.png';
 
 class App extends React.Component {
+
     constructor(props){
         super(props);
         this.state = {
             f5MockJson: "Loading...",
             payloadJson: "",
-            results: []
+            payloadValid: true,
+            uiLocked: false
         };
 
         this.fetchF5Mock();
@@ -19,20 +21,82 @@ class App extends React.Component {
 
      async fetchF5Mock() {
         let result = await axios.get(window.location.origin + '/api/f5Config');
-        this.setState({f5MockJson: JSON.stringify(result.data, null, 4)});
-    };
+        this.setState({
+            f5MockJson: JSON.stringify(result.data, null, 4)
+        });
+    }
+
+    updateConfig(event){
+        this.setState({
+            payloadJson: event.target.value
+        });
+    }
+
+    copyConfig(){
+        this.setState({
+            payloadJson: this.state.f5MockJson
+        });
+    }
+
+    clearConfig(){
+        this.setState({
+            payloadJson: "",
+            payloadValid: true
+        });
+    }
+
+    async execute(){
+        let payloadString = this.state.payloadJson;
+        let isConfigValid = this.validateConfig(payloadString);
+        if(isConfigValid) {
+            this.setState({
+                payloadValid: true,
+                uiLocked: true
+            });
+
+            let payloadJson = JSON.parse(payloadString);
+            payloadJson.results = {};
+            let result = await axios.post(window.location.origin + '/api/execute', payloadJson);
+
+            console.log("execution result:", result.data);
+            this.setState({
+                payloadJson: JSON.stringify(result.data, null, 4),
+                uiLocked: false
+            });
+        }else{
+            this.setState({
+                payloadValid: false
+            });
+            console.log("failed to parse execution payload");
+        }
+    }
+
+    validateConfig(jsonString){
+        try{
+            let parsedConfig = JSON.parse(jsonString);
+            if(parsedConfig.hasOwnProperty("devices")
+                && parsedConfig.hasOwnProperty("operation")
+                && parsedConfig.hasOwnProperty("results")){
+                return true;
+            }else{
+                return false;
+            }
+        }catch(e){
+            return false;
+        }
+    }
 
     render() {
         return (
             <div className="app">
-                <h1>Operation Engine user interface!</h1>
+                <h1>Operation Engine user interface</h1>
                 <div className="container bg-dark contents">
                     <div className="card">
                         <div className="card-img-wrapper">
                             <img className="card-img-top" src={f5_logo} alt="F5 logo" />
                         </div>
                         <div className="card-body">
-                            <textarea className="form-control" value={this.state.f5MockJson} />
+                            <textarea disabled className="form-control" value={this.state.f5MockJson} />
                         </div>
                     </div>
 
@@ -40,12 +104,19 @@ class App extends React.Component {
                         <div className="card-img-wrapper">
                             <img className="card-img-top" src={engine_logo} alt="Engine logo" />
                         </div>
-                        <div className="card-body with-buttons">
-                            <textarea  className="form-control">This text area should contain operation results</textarea>
+                        <div className={this.state.payloadValid? "card-body with-buttons" : "card-body with-buttons border-danger"}>
+                            <textarea disabled={this.state.uiLocked} className="form-control" placeholder="This text area should contain operation results" value={this.state.payloadJson} onChange={this.updateConfig.bind(this)} />
                             <div className="btn-group">
-                                <button type="button" className="btn btn-primary">Copy</button>
-                                <button type="button" className="btn btn-primary">Clear</button>
-                                <button type="button" className="btn btn-primary">Execute</button>
+                                <button type="button" className="btn btn-primary" disabled={this.state.uiLocked} onClick={this.copyConfig.bind(this)}>Copy</button>
+                                <button type="button" className="btn btn-primary" disabled={this.state.uiLocked} onClick={this.clearConfig.bind(this)}>Clear</button>
+                                {
+                                    this.state.uiLocked?
+                                    <button type="button" className="btn btn-primary" disabled>
+                                        <span className="spinner-border spinner-border-sm" />
+                                        <span>Running...</span>
+                                    </button>:
+                                    <button type="button" className="btn btn-primary" onClick={this.execute.bind(this)}>Execute</button>
+                                }
                             </div>
                         </div>
                     </div>
