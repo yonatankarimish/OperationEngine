f4bd3ea27bca3a76fc8cb325cef4b3d84dfcd37b
Yonatan <yonatan@backbox.co>
2020-02-10 12:11:41 +0200

    Web UI made more responsive for use in mobile devices Added parcel .cache directory to gitignore

--- web/app/app.js
+++ web/app/app.js
@@ -28,16 +28,25 @@ class App extends React.Component {
                 <h1>Operation Engine user interface!</h1>
                 <div className="container bg-dark contents">
                     <div className="card">
-                        <img className="card-img-top" src={f5_logo} alt="F5 logo" />
+                        <div className="card-img-wrapper">
+                            <img className="card-img-top" src={f5_logo} alt="F5 logo" />
+                        </div>
                         <div className="card-body">
                             <textarea className="form-control" value={this.state.f5MockJson} />
                         </div>
                     </div>
 
                     <div className="card">
-                        <img className="card-img-top" src={engine_logo} alt="F5 logo" />
-                        <div className="card-body">
+                        <div className="card-img-wrapper">
+                            <img className="card-img-top" src={engine_logo} alt="Engine logo" />
+                        </div>
+                        <div className="card-body with-buttons">
                             <textarea  className="form-control">This text area should contain operation results</textarea>
+                            <div className="btn-group">
+                                <button type="button" className="btn btn-primary">Copy</button>
+                                <button type="button" className="btn btn-primary">Clear</button>
+                                <button type="button" className="btn btn-primary">Execute</button>
+                            </div>
                         </div>
                     </div>
                 </div>
